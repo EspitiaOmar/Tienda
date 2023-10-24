1bbf2a4ba27850183132380554832aecb436b54d
EspitiaOmar <espitiaomar1110@gmail.com>
2023-10-24 15:38:15 -0500

    crud producto

--- src/routes/consumidor.js
+++ src/routes/consumidor.js
@@ -2,10 +2,16 @@ const { Router } = require("express");
 const router = Router();
 const userSchema = require("../models/user");
 
+router.get("/consumidor", async (req, res) => {
+  res.render("usuario");
+});
+
 router.post("/consumidor/add", async (req, res) => {
   try {
     const usuario = userSchema(req.body);
     await usuario.save();
+    const usuarios = await userSchema.find().lean();
+    res.render("usuariosList", { usuarios: usuarios });
   } catch (error) {
     console.log(error);
   }
@@ -25,5 +31,12 @@ router.post("/consumidor/edit/:id", async (req, res) => {
   await userSchema.findByIdAndUpdate(req.params.id, req.body);
   res.render("usuariosList");
 });
+
+router.get("/consumidor/delete/:id", async (req, res) => {
+  await userSchema.findByIdAndDelete(req.params.id, req.body);
+  res.render("usuariosList");
+});
+
+
 
 module.exports = router;
